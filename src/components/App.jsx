e32fddfce5997ee37f04810ff110a6af807eb195
Elena Berezina <farfalina1908@gmail.com>
2023-05-19 18:06:31 +0300

    made refactoring by hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,93 +1,78 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import ContactsList from './Phonebook/ContactsList/ContactsList';
 import Filter from './Phonebook/Filter/Filter';
 import Form from './Phonebook/Form/Form';
 import { nanoid } from 'nanoid/non-secure';
 import css from './Phonebook.module.css';
 
-export const App = () => {
-  return (
-    <div>
-      <Phonebook />
-    </div>
-  );
-};
+function App() {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
 
-class Phonebook extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-
-  componentDidMount() {
+  useEffect(() => {
     const localData = localStorage.getItem('contacts');
     if (localData) {
-      this.setState({ contacts: JSON.parse(localData) });
+      setContacts(JSON.parse(localData));
     }
-  }
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  changeFilter = event => {
-    this.setState({
-      filter: event.currentTarget.value,
-    });
+  const changeFilter = event => {
+    setFilter(event.currentTarget.value);
   };
 
-  formSubmitHandler = data => {
+  const formSubmitHandler = data => {
     const newContact = {
       id: nanoid(),
       name: data.name,
       number: data.number,
     };
 
-    this.state.contacts.find(
+    contacts.find(
       contact => contact.name.toLowerCase() === data.name.toLowerCase()
     )
       ? alert(`${data.name} is already in contacts`)
-      : this.setState({
-          contacts: [...this.state.contacts, newContact],
-        });
+      : setContacts([...contacts, newContact]);
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
-
-  render() {
-    const normalizedFilter = this.state.filter.toLowerCase();
-    const visibleContacts = this.state.contacts.filter(contact => {
-      return contact.name.toLowerCase().includes(normalizedFilter);
-    });
-    return (
-      <div className={css.container}>
-        <Form onSubmit={this.formSubmitHandler} />
-        <ContactsList
-          title="Contacts"
-          contacts={visibleContacts}
-          onDeleteContact={this.deleteContact}
-        >
-          <Filter
-            onChange={this.changeFilter}
-            type="text"
-            value={this.state.filter}
-            name="filter"
-            title=""
-            pattern=""
-          />
-        </ContactsList>
-      </div>
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
     );
-  }
+  };
+
+  const normalizedFilter = filter.toLowerCase();
+  const visibleContacts = contacts.filter(contact => {
+    return contact.name.toLowerCase().includes(normalizedFilter);
+  });
+
+  return (
+    <div className={css.container}>
+      <Form onSubmit={formSubmitHandler} />
+      <ContactsList
+        title="Contacts"
+        contacts={visibleContacts}
+        onDeleteContact={deleteContact}
+      >
+        <Filter
+          onChange={changeFilter}
+          type="text"
+          value={filter}
+          name="filter"
+          title=""
+          pattern=""
+        />
+      </ContactsList>
+    </div>
+  );
 }
+
+export default App;
--- src/components/Phonebook/Form/Form.jsx
+++ src/components/Phonebook/Form/Form.jsx
@@ -1,68 +1,69 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Input from '../Input/Input';
 import Label from '../Label/Label';
 import css from './Form.module.css';
 
-class Form extends Component {
-  state = {
-    name: '',
-    number: '',
+const Form = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  handleChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    const { name, number } = this.state;
-    this.props.onSubmit({
+    onSubmit({
       name,
       number,
     });
-    this.setState({
-      name: '',
-      number: '',
-    });
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    const { name, number } = this.state;
-    return (
-      <>
-        <h1 className={css.mainTitle}>Phonebook</h1>
-        <form className={css.form} onSubmit={this.handleSubmit}>
-          <Label text="Name">
-            <Input
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              value={name}
-              onChange={this.handleChange}
-            />
-          </Label>
-          <Label text="Number">
-            <Input
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              value={number}
-              onChange={this.handleChange}
-            />
-          </Label>
-          <button className={css.formButton} type="submit">
-            Add
-          </button>
-        </form>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <h1 className={css.mainTitle}>Phonebook</h1>
+      <form className={css.form} onSubmit={handleSubmit}>
+        <Label text="Name">
+          <Input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            value={name}
+            onChange={handleChange}
+          />
+        </Label>
+        <Label text="Number">
+          <Input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            value={number}
+            onChange={handleChange}
+          />
+        </Label>
+        <button className={css.formButton} type="submit">
+          Add
+        </button>
+      </form>
+    </>
+  );
+};
+
 Input.propTypes = {
   onChange: PropTypes.func.isRequired,
   value: PropTypes.string.isRequired,
@@ -71,4 +72,5 @@ Input.propTypes = {
   title: PropTypes.string.isRequired,
   pattern: PropTypes.string.isRequired,
 };
+
 export default Form;
